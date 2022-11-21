726178d4f8b9db5a6df5c011b6f5cd9c64e569c1
hyadav-x <hitesh2@illinois.edu>
2022-11-21 09:12:14 -0600

    [HY] Added code to return prediction for a given tweet.

---
+++ src/get_sentiments.py
@@ -0,0 +1,29 @@
+import pickle
+import pandas as pd
+from data_preprocessing import *
+
+# Vector and model filenames
+vector_filename = 'textVetorizer.pickle'
+model_filename = 'lrModel_trained.pickle'
+
+# Read the saved vecto and model files
+vectorizer = pickle.load(open(vector_filename, 'rb'))
+model = pickle.load(open(model_filename, 'rb'))
+
+def get_sentiment(tweet: str):
+    tweetDataset = pd.DataFrame([[0, tweet]], columns=['target', 'text'])
+
+    tweetDataset = clean_data(tweetDataset)
+
+    predictFor = tweetDataset['text']
+
+    # Create vector
+    predictForVectorized = vectorizer.transform(predictFor)
+
+    prediction = model.predict(predictForVectorized)
+
+    print("Prediction for tweet '{}': {}".format(tweet, prediction))
+
+    return prediction
+
+get_sentiment('this is a good tweet')
